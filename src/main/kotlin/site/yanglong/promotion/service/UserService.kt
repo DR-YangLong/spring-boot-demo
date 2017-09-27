9dcbec05143adc9e1c4a90bc0090ca344c93e74e
杨龙 <410357434@163.com>
2017-09-27 14:16:32 +0800

    添加autoload cache

--- src/main/kotlin/site/yanglong/promotion/service/UserService.kt
+++ src/main/kotlin/site/yanglong/promotion/service/UserService.kt
@@ -1,6 +1,7 @@
 package site.yanglong.promotion.service
 
 import com.baomidou.mybatisplus.service.IService
+import com.jarvis.cache.annotation.Cache
 import site.yanglong.promotion.model.UserBase
 
 /**
@@ -11,5 +12,6 @@ import site.yanglong.promotion.model.UserBase
  * @version 1.0    2017/7/13
  */
 interface UserService :IService<UserBase>{
+    @Cache(expire = 500,key = "'findUserByNamePage:'+#args[0]+':'+#args[1]+':'+#args[2]",autoload = true,requestTimeout = 18000)
     fun findByName(name:String,page:Int,size:Int):List<UserBase>
 }
